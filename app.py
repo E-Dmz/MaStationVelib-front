8af8e52632292f6d72bdd765a5009acd594c38bf
Etienne Doumazane <etienne.doumazane@gmail.com>
2021-12-18 19:16:56 +0100

    first figure

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
 import streamlit as st
 import requests
+import pandas as pd
+import matplotlib.pyplot as plt
 
-API_URL = 'https://containerapi-vxe2f26aka-ew.a.run.app/predict_hits'
+"Hello world!"
+API_URL = 'https://velib-v1-hi7so7se7a-ew.a.run.app/station'
+
+
+
+
+code_station = st.text_input('code de la station')
+
+query = {
+    'station': code_station,
+}
+
+if code_station:
+    response = requests.get(API_URL, params=query).json()
+
+    df = pd.DataFrame(response)
+    fig, ax=plt.subplots()
+
+    ax.plot(df.datetime, df.meca)
+
+
+    st.pyplot(fig)
